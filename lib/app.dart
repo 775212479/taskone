20a62b0abc2c43b88d416a97498956755f454003
775212479 <132310121+775212479@users.noreply.github.com>
2023-06-07 20:56:48 +0300

    Task One

---
+++ lib/app.dart
@@ -0,0 +1,17 @@
+
+
+
+import 'package:flutter/material.dart';
+
+import 'splashscreen.dart';
+
+class pageOne extends StatelessWidget {
+  const pageOne({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: SplashScreen() ,
+    );
+  }
+}
